cc3af3b618672dd8c7a60d82326a8a12ec462b26
NaldoColella <naldo.colella@gmail.com>
2021-05-04 14:35:37 -0300

    Adding params, and starting basic validation in models

--- back-end-api/app/models/address.rb
+++ back-end-api/app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :municipe
+  validates :cep, :logradouro, :bairro, :cidade, presence: true
+  validates :uf , length: { is: 2 }, presence: true
+  #:complemento
+  #:ibge
 end
--- back-end-api/app/models/municipe.rb
+++ back-end-api/app/models/municipe.rb
@@ -1,2 +1,9 @@
 class Municipe < ApplicationRecord
+    has_one :address
+    accepts_nested_attributes_for :address
+    
+    validates :cpf, length: { is: 11 } 
+    validates :full_name, :cpf, :phone, :photo, :status, presence: true
+    validates :mail, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates_associated :address
 end
