41363e680083d49b90f581b373e3479a973fa975
NaldoColella <naldo.colella@gmail.com>
2021-05-07 16:36:37 -0300

    [FE] - adding footer in default layout

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <header id="topo">
-
-    </header>
+    <header id="topo"></header>
     <Nuxt />
+    <footer id="rodape">
+      <b-container>
+        <p>OM30 - Todos os direitos reservados</p>
+      </b-container>
+    </footer>
   </div>
 </template>
 
 <style>
-header#topo{
+header#topo {
   background: #007bff;
   width: 100%;
   height: 100px;
   margin-bottom: 50px;
 }
+footer#rodape{
+  margin-top: 35px;
+  background: #007bff;
+  width: 100%;
+  padding: 25px 0px;
+}
+footer#rodape p{
+  margin-bottom: 0px;
+  color: #fff;
+}
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -41,11 +47,11 @@ html {
 }
 
 .table .thead-light th {
-    color: #fff;
-    background-color: #2c88ea;
-    border-color: #dee2e6;
+  color: #fff;
+  background-color: #2c88ea;
+  border-color: #dee2e6;
 }
-table.b-table .b-table-busy-slot td{
+table.b-table .b-table-busy-slot td {
   padding: 60px 0px;
 }
 </style>
