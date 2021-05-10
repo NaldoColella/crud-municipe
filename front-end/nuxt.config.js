f4255001c330be9258c01e329c32124a27bee8cf
NaldoColella <naldo.colella@gmail.com>
2021-05-10 16:21:21 -0300

    [FE] - Adding awesome-mask and vee-validate

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/awesome-mask.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -50,6 +51,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    
   },
 
   eslint: {
